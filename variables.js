76d15dbd17e17199d3280d963293d34c1e057d9f
Teerth554 <132830420+Teerth554@users.noreply.github.com>
2024-04-14 22:49:43 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,16 @@
+const accountId=455001
+let accountEmail="teerth@gmail.com"
+var accountPassword="789465"
+accountCity="dewas"
+let accountState;
+//accountId=4638//not allowed bcz its a constant
+accountEmail="ygruevb@gmail.com"
+accountCity="indore"
+
+/*
+prefer not to  use var 
+bcz of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
