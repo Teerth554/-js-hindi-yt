0781f4c0a175ad744b48ab46072efeffeb6eca59
Teerth554 <132830420+Teerth554@users.noreply.github.com>
2024-04-14 22:44:37 +0530

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,27 @@
+const name="teerth "
+const repo=77
+//console.log(name + repo +' value');
+
+console.log(`Hello my name is ${name} and my current repo is ${repo}`);
+const gameName=new String("tgywvWhb")
+// console.log(gameName);
+// console.log(gameName[2]);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('e'));
+// console.log(gameName.charAt(5));
+
+const newString =gameName.substring(0,5)
+console.log(newString);
+const anotherString =gameName.slice(-7,5)
+console.log(anotherString);
+
+const newm="           memo       "
+console.log(newm.trim())
+
+const url="https://teertb%20.com/teerth"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes("teerth "))
