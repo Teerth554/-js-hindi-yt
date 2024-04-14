0da72cf756999ea25129a0e477eb1d43362fa0c2
Teerth554 <132830420+Teerth554@users.noreply.github.com>
2024-04-14 22:49:13 +0530

    Create conversions.js

---
+++ conversions.js
@@ -0,0 +1,38 @@
+let score=false
+console.log(typeof score);
+console.log(typeof (score));
+
+let valInNumber =Number(score)   //typecasting
+console.log(valInNumber);
+console.log(typeof valInNumber);
+/*
+
+"33"=>33
+"33abd"=>NaN
+*/
+
+let isLoggedIn =1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//****************************OPERATIONS************************//
+let  val=7
+let nefgValue=-val
+console.log(2%3 )
+console.log(2**6 )
+console.log( 5-6)
+
+let str1="vaidehee"
+let str2=" teerth"
+let str3=str1+str2
+console.log(str3)
+
+console.log( "1"+2)  //->12
+console.log( 1+"2"+2)  //->122
+console.log( 1+2+"2")  //->32
+
+/*
+just to avoid confusion just use parenthesis()
+*/
+
+
+
